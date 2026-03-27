060c1e7d4bc828763397b615b662c94386c679d1
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2026-03-27 14:01:20 +0000

    asd

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,19 @@ NETWORK_NAME=estagios-network
 NETWORK_NAME_NGINX=fabrica-nginx-proxy-network
 
 # Navegar até a pasta onde está o docker-compose
-cd E-Stagio\ Prod/
+# cd E-Stagio\ Prod/
 
 # Parar os containers em execução
 docker compose down
 
 # Voltar para a pasta anterior para fazer o git pull
-cd ..
+# cd ..
 
 # Puxar as últimas mudanças do GitHub
 git pull origin main
 
 # Navegar novamente para a pasta do docker-compose
-cd E-Stagio\ Prod/
+# cd E-Stagio\ Prod/
 
 
 # Subir os containers novamente com as novas mudanças
